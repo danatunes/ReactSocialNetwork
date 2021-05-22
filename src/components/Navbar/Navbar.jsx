8439afce42b632382318b2d8e43a6210ed6898cb
Magzhan <codebuster@bk.ru>
2021-05-22 12:37:29 +0500

    NavLink activeClassName / page dont reloaded every time

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,23 +1,24 @@
  import s from "./Navbar.module.css";
+ import {NavLink} from "react-router-dom";
 
 
  const Navbar = () => {
      return(
         <nav className={s.nav}>
         <div className={s.item}>
-          <a href='/profile'>Profile</a>
+          <NavLink to='/profile' activeClassName={s.active}>Profile</NavLink>
         </div>
         <div className={s.item}>
-        <a href='/dialogs'>Dialogs</a>
+        <NavLink to='/dialogs' activeClassName={s.active}>Dialogs</NavLink>
         </div>
         <div className={s.item}>
-        <a href='/news'>News</a>
+        <NavLink to='/news' activeClassName={s.active}>News</NavLink>
         </div>
-        <div className={`${s.item} ${s.active}`}>
-        <a href='/music'>Music</a>
+        <div className={`${s.item}`}>
+        <NavLink to='/music' activeClassName={s.active}>Music</NavLink>
         </div>
         <div className={s.item}>
-        <a href='/settings'>Settings</a>
+        <NavLink to='/settings' activeClassName={s.active}>Settings</NavLink>
         </div>
       </nav>
      );
